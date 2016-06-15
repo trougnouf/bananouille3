51ce2b65acc77c2cfc2b0141af07d43d95eb1e91
Benoit Brummer <trougnouf@localhost.localdomain>
2016-06-14 19:49:15 -0600

    Ate a delicious sandwich, walked around

--- blog/adventures.php
+++ blog/adventures.php
@@ -4,40 +4,38 @@ if($_GET["a"])			$_SESSION["a"] = $_GET["a"];
 else if(!$_SESSION["a"])	$_SESSION["a"] = randomAdventure();
 
 ?>
-
+ <!DOCTYPE html> 
 <html>
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="../style.css" />
+		<link rel="stylesheet" href="blog.css" />
 		<title>Bananouille³</title>
 	</head>
 	<body>
 		<header>
 			<a href="../"><img src="../img/logo.svg" style="float:left;" /></a>
-			<div style="background-color:black;min-width:5px;min-height:150px;float:left;margin-left:10px;margin-right:10px;"></div> <!--black bar-->
-			<img src="adventures/<?php echo $_SESSION["a"]; ?>.png" style="float-left;" />
-			<a href="">Random adventure</a>, <a href="?a=latest">Latest adventure</a>, <a href="?a=list">List</a>
+			<div style='background-image:url("adventures/<?php echo $_SESSION["a"]; ?>.png");width:100%;'></div>
+
 		</header>
+		<p class="blogmenu"><a href="">Random adventure</a>, <a href="?a=latest">Latest adventure</a>, <a href="?a=list">List</a></p>
 		<section id="content">
-		
+		<?php
+		include "adventures/{$_SESSION["a"]}.php";
+		?>		
 		
 		</section>
 	</body>
 </html>
 
-<div style="float:left;">
-<br />
 
-
-
-</div>
 <?php
 /*
 // TODO: include "adventures/$_GET["a"]
 $_SESSION["a"] = "megaroadtrip";
 include "adventures/megaroadtrip.php";
 */
-include "adventures/{$_SESSION["a"]}.php";
+
 ?>
 
 <?php
--- blog/adventures/megaroadtrip.php
+++ blog/adventures/megaroadtrip.php
@@ -1,21 +1,38 @@
 <div>
 
 
-<h2 style="float:left;">Megaroadtrip 2016-05-23 - 2016-07-29</h2>
-<p>I decided to go on a 70-days long road trip around the USA after graduating college. I am exploring what is left of Florida before moving back to Belgium.</p>
+<h1 style="float:left;margin:1px;">Megaroadtrip</h1>
+<h2 style="margin:0px;">2016-05-23 - 2016-07-29</h2>
+<h3 style="margin:0px">FL, LO, TX, NM, AZ, UT, CO, ...</h3>
+<p style="margin-top:3px;">Megaroadtrip is a 70-days road trip destined to cover what is left of Florida before I leave the USA.</p>
 
-<div
+<h3>2016-05-23</h3>
 
-<div style="float:left;">
-	<h3>2016-05-23</h3>
-	
-	<img src="adventures/megaroadtrip/premrt/GEDC9174.JPG" style="max-width: 33%;" />	<img src="adventures/megaroadtrip/premrt/GEDC9179.JPG" style="max-width: 33%;" />	<img src="adventures/megaroadtrip/premrt/GEDC9177.JPG" style="max-width: 32%;" />
-
-	<img src="adventures/megaroadtrip/2016-05-23/IMG_20160523_220003.jpg" style="max-width: 49%;max-height:100%;" />
-	<p style="float:right;display:inline-block;">I sold my beloved 2005 Yamaha FZ6 a few days before the trip, used that money to ship way too many things to Belgium, and hopped into Alex's Mazda Speed 3 for whatever will ensue. </p>
-	<img src="adventures/megaroadtrip/2016-05-23/IMG_20160523_232630.jpg" style="max-width: 50%;max-height:100%;" />
-
+<div class="flexbox">
+	<div><a href="adventures/megaroadtrip/premrt/GEDC9174.JPG"><img src="adventures/megaroadtrip/premrt/GEDC9174.JPG" /></a></div>
+	<div><a href="adventures/megaroadtrip/premrt/GEDC9179.JPG"><img src="adventures/megaroadtrip/premrt/GEDC9179.JPG"/></a></div>
+	<div><a href="adventures/megaroadtrip/premrt/GEDC9177.JPG"><img src="adventures/megaroadtrip/premrt/GEDC9177.JPG"/></a></div>
+</div>
+<div class="flexbox">
+	<div><a href="adventures/megaroadtrip/2016-05-23/IMG_20160523_220003.jpg"><img src="adventures/megaroadtrip/2016-05-23/IMG_20160523_220003.jpg" /></a></div>
+		<div class="verticalbox">
+			<div><p style="display:inline-block;">I sold my beloved 2005 Yamaha FZ6 a few days before the trip, used that money to ship way too many things to Belgium, and hopped into Alex's Mazda Speed 3 for whatever will ensue. </p></div>
+			<div><a href="adventures/megaroadtrip/2016-05-23/IMG_20160523_232630.jpg"><img src="adventures/megaroadtrip/2016-05-23/IMG_20160523_232630.jpg" /></a></div>
+		</div>
+	</div>
 </div>
 
+<?php
+/*
+[t:h1]
+[[i:][i:][i:]]
+[[i:][v:[""][i:]]]
 
-</div>
+replace
+[t:2016-06-14] w/ <h3>2016-06-14</h3>
+
+
+*/
+?>
+
+
